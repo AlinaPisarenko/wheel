ef3a78910dfd661cb2ea816e8d68e078c810fef7
Alina <pisarenko.alina@gmail.com>
2022-06-20 23:44:00 -0400

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Wheel from "./Wheel";
+import PersonalPage from "./PersonalPage";
+import Navigation from "./Navigation";
+import "./sass/main.scss";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Wheel />} />
+        <Route path="/home" element={<PersonalPage />} />
+      </Routes>
     </div>
   );
 }
